28ecfbc089b5f0a5b281c241fca5b513cb1982f6
Nikolay Igorev <frololo.frololo@yandex.ru>
2022-04-21 23:11:33 +0700

    add docker

---
+++ docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+echo -e "Wait for database\n"
+POSTGRES_TCP=$(echo "$DATABASE_URL" | sed 's/^postgres/tcp/')
+dockerize -wait "$POSTGRES_TCP" -timeout 20s
+
+if [[ "$1" = "test" ]]; then
+    echo -e "Running tests\n"
+    python manage.py test
+else
+    echo -e "Migrating database\n"
+    python manage.py migrate
+
+    echo -e "Starting server\n"
+    gunicorn test_comments.wsgi
+fi
